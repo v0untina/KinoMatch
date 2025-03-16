21b95c4457347922ead206235715c060aea76878
prxductive <144677265+prxductive@users.noreply.github.com>
2025-03-16 12:08:28 +0300

    Add files via upload

---
+++ frontend/src/widgets/ContactsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './extra_info.css';
+
+const ContactsPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Контакты</h1>
+            <p>Наши контакты...</p>
+        </div>
+    );
+};
+
+export default ContactsPage;
